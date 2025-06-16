cd791e9fc7d541d359671fc4e5d65de8417d8b54
Alex van Steenhoven <alex@modus-digital.com>
2025-06-16 10:30:27 +0200

    Add SAML authentication configuration file
    - Introduced a new configuration file for SAML authentication at config/saml-auth.php.
    - Defined the authentication mode with options for single-tenant and multi-tenant setups.
    This file serves as a foundational component for implementing SAML authentication in the application.

---
+++ config/saml-auth.php
@@ -0,0 +1,14 @@
+<?php
+
+// config for ModusDigital/LaravelSamlAuth
+return [
+
+    /**
+     * The mode of the SAML authentication.
+     *
+     * Choose between:
+     * - single-tenant
+     * - multi-tenant
+     */
+    'mode' => 'single-tenant',
+];
