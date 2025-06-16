a118d39c4bcde9088ed00c48db7e7a95ac195948
Alex van Steenhoven <alex@modus-digital.com>
2025-06-16 10:29:46 +0200

    Update package metadata and documentation for laravel-saml-auth
    - Changed package name and description in composer.json.
    - Updated author information and copyright in LICENSE.md.
    - Modified README.md to reflect new package details and added installation instructions.
    - Adjusted test suite name in phpunit.xml.dist.
    - Updated URLs in issue templates and funding configuration.
    - Added package banner image.
    - Refactored namespaces in test files to match new package structure.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace ModusDigital\LaravelSamlAuth\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use ModusDigital\LaravelSamlAuth\LaravelSamlAuthServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -13,14 +13,14 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'ModusDigital\\LaravelSamlAuth\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            LaravelSamlAuthServiceProvider::class,
         ];
     }
 
